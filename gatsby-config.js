3795f6b586c74108cc495d55cbf31df95310b686
Piotr Krasny <krasny.piotr@gmail.com>
2021-01-01 18:59:21 +0100

    Try to optimize css

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,6 +11,33 @@ module.exports = {
   plugins: [
     `gatsby-plugin-transition-link`,
     `gatsby-plugin-sass`,
+    // {
+    //   resolve: `gatsby-plugin-postcss`,
+    //   options: {
+    //     postCssPlugins: [require(`postcss-preset-env`)({ stage: 0 })],
+    //   },
+    // },
+    {
+      resolve: `gatsby-plugin-purgecss`,
+      options: {
+        printRejected: true, // Print removed selectors and processed file names
+        develop: true, // Enable while using `gatsby develop`
+        // tailwind: true, // Enable tailwindcss support
+        // whitelist: ["whitelist"], // Don't remove this selector
+        // ignore: ["src/styles/carousel/", "prismjs/", "docsearch.js/"], // Ignore files/folders
+        // ignore: ["src/styles/carousel/"], // Ignore files/folders
+        // ignore: [
+        //   "slick-carousel",
+        //   "Temp_00_MainSiteLayout.js",
+        //   "MDBNavbar",
+        //   "MDBNavbarNav",
+        //   "MDBNavbarToggler",
+        //   "MDBCollapse",
+        // ], // Ignore files/folders
+        // purgeOnly: ["components/", "/main.css", "bootstrap/"], // Purge only these files/folders
+        purgeOnly: ["all.min.css", "mdb.css"], // Purge only these files/folders
+      },
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
