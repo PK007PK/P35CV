75681061d1f645d6715346e5e5b546848b70408f
Piotr Krasny <krasny.piotr@gmail.com>
2021-01-01 19:44:13 +0100

    test

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,21 +21,19 @@ module.exports = {
       resolve: `gatsby-plugin-purgecss`,
       options: {
         printRejected: true, // Print removed selectors and processed file names
-        develop: true, // Enable while using `gatsby develop`
+        develop: false, // Enable while using `gatsby develop`
         // tailwind: true, // Enable tailwindcss support
         // whitelist: ["whitelist"], // Don't remove this selector
         // ignore: ["src/styles/carousel/", "prismjs/", "docsearch.js/"], // Ignore files/folders
         // ignore: ["src/styles/carousel/"], // Ignore files/folders
-        // ignore: [
-        //   "slick-carousel",
-        //   "Temp_00_MainSiteLayout.js",
-        //   "MDBNavbar",
-        //   "MDBNavbarNav",
-        //   "MDBNavbarToggler",
-        //   "MDBCollapse",
-        // ], // Ignore files/folders
+        ignore: [
+          "MDBNavbar",
+          "MDBNavbarNav",
+          "MDBNavbarToggler",
+          "MDBCollapse",
+        ], // Ignore files/folders
         // purgeOnly: ["components/", "/main.css", "bootstrap/"], // Purge only these files/folders
-        purgeOnly: ["all.min.css", "mdb.css"], // Purge only these files/folders
+        // purgeOnly: ["all.min.css", "mdb.css"], // Purge only these files/folders
       },
     },
     {
