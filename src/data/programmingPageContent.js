d1489267cde7f5e05003de89b70845202d5ff165
Piotr Krasny <krasny.piotr@gmail.com>
2021-01-13 11:55:39 +0100

    Fix spelling

--- src/data/programmingPageContent.js
+++ src/data/programmingPageContent.js
@@ -6,7 +6,7 @@ export const programmingPageContent = {
   title: ["Programowanie", "Programming"],
   description: [
     "Od 2019 roku uczę się programowania. Interesuje mnie JAM Stack (Javascript-APIs-Markup). Moją ulubioną technologią jest React.js",
-    "I have been learning programming since 2019. I am interested in JAM Stack (Javascript-APIs-Markup). My favourite technology is React.js",
+    "I have been learning programming since 2019. I am interested in JAM Stack (Javascript-APIs-Markup). My favorite technology is React.js",
   ],
 }
 
