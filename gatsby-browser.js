1cc43fca48ed38c04dbfd3f18f79194dac48686f
Piotr Krasny <krasny.piotr@gmail.com>
2021-03-25 10:49:27 +0100

    Change directory structure

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import "./src/normalize.css"
+import "./src/styles/normalize.css"
 
 import "@fortawesome/fontawesome-free/css/all.min.css"
 import "bootstrap-css-only/css/bootstrap.min.css"
-import "./src/assets/scss/mdb-free.scss"
+import "./src/styles/scss/mdb-free.scss"
 
 import { AppProvider } from './src/AppProvider';
 
