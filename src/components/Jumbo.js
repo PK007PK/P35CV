9ee0ae54aeb6296c8e05be5e1930fcebd0e10c56
Piotr Krasny <krasny.piotr@gmail.com>
2021-04-28 13:31:29 +0200

    Save

--- src/components/Jumbo.js
+++ src/components/Jumbo.js
@@ -1,5 +1,5 @@
-import React, { useContext } from "react"
-import Img from "gatsby-image"
+import React, { useContext } from 'react';
+import Img from 'gatsby-image';
 
 import AppContext from '../AppProvider';
 
@@ -13,34 +13,35 @@ const Jumbo = ({
   imgSource,
 }) => {
   const { print } = useContext(AppContext);
-  const Text = text
-  const RightBox = rightBox
-  const BottomBar = bottomBar
-  
+  const Text = text;
+  const RightBox = rightBox;
+  const BottomBar = bottomBar;
+
   return (
     <div className="jumbotron" style={style}>
-      <div className={print ? "container mt-2" : "container mt-5"}>
+      <div className={print ? 'container mt-2' : 'container mt-5'}>
         <div
           className={
             print
-              ? "row justify-content-between flex-row-reverse"
-              : "row justify-content-between"
+              ? 'row justify-content-between flex-row-reverse'
+              : 'row justify-content-between'
           }
-          style={{ alignItems: "center" }}
+          style={{ alignItems: 'center' }}
         >
-          <div className={print ? "col-12 col-md-7 text-left" : "col-12 col-md-7 text-right"}>
+          <div
+            className={
+              print ? 'col-12 col-md-7 text-left' : 'col-12 col-md-7 text-right'
+            }
+          >
             <h1 className="h1">
               {title}
               <br />
               <span className="h3 font-weight-lighter">{subtitle}</span>
             </h1>
-            {typeof text === "string" && <p className="my-4">{text}</p>}
-            {typeof text === "object" ||
-              (typeof text === "function" && <Text />)}
-            <hr
-              className=""
-              style={{ width: "100%", marginInlineEnd: 0 }}
-            />
+            {typeof text === 'string' && <p className="my-4">{text}</p>}
+            {typeof text === 'object' ||
+              (typeof text === 'function' && <Text />)}
+            <hr className="" style={{ width: '100%', marginInlineEnd: 0 }} />
             {bottomBar && <BottomBar />}
           </div>
           <div className="col-12 col-md-4 mt-4 mt-md-0">
@@ -54,7 +55,7 @@ const Jumbo = ({
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Jumbo
+export default Jumbo;
